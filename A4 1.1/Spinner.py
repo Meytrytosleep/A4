ee96cad67d5335d10ae81017d94d2af0e4fe091d
Meytrytosleep <145814468+Meytrytosleep@users.noreply.github.com>
2023-10-03 22:40:01 -0600

    FinalUpdate Spinner.py

--- A4 1.1/Spinner.py
+++ A4 1.1/Spinner.py
@@ -7,15 +7,19 @@ class Spinner:
 
     def _load_synonyms(self, filename):
         synonyms = {}
-        with open(filename, 'r') as f:
-            for line in f.readlines():
-                key, values = line.strip().split(':')
-                synonyms[key] = values.split(', ')
+        try:
+            with open(filename, 'r') as f:
+                for line in f.readlines():
+                    key, values = line.strip().split(':')
+                    synonyms[key] = values.split(', ')
+        except Exception as e:
+            print(f"Error reading synonym file: {e}")
         return synonyms
 
     def spin_word(self, word):
         if word in self.synonyms and random.randint(1, 100) > self.spin_probability:
-            return random.choice(self.synonyms[word])
+            synonyms = [syn for syn in self.synonyms[word] if syn != word]
+            return random.choice(synonyms) if synonyms else word
         return word
 
     def spin_text(self, text):
