210f2af5a506cb5a6bffcf31ea2bbb46db9713d9
Meytrytosleep <145814468+Meytrytosleep@users.noreply.github.com>
2023-10-03 21:01:52 -0600

    Update1 SpinFile.py

--- A4 1.1/SpinFile.py
+++ A4 1.1/SpinFile.py
@@ -3,9 +3,9 @@ import string
 def clean_text(text):
     return text.translate(str.maketrans('', '', string.punctuation)).lower()
 
-def main():
-    spinner = Spinner('test-synonyms.txt')
-    with open('essay.txt', 'r') as f:
+def main(synonym_file='test-synonyms.txt', essay_file='essay.txt'):
+    spinner = Spinner(synonym_file)
+    with open(essay_file, 'r') as f:
         original_text = clean_text(f.read())
         print("Original:", original_text)
         for i in range(3):
