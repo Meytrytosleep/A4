dddeb9476a2a27b9da021f965e97403b1280354f
Meytrytosleep <145814468+Meytrytosleep@users.noreply.github.com>
2023-10-03 21:19:25 -0600

    FinalUpdate SpinFile.py

--- A4 1.1/SpinFile.py
+++ A4 1.1/SpinFile.py
@@ -7,20 +7,24 @@ def clean_text(text):
 def main(synonym_file='test-synonyms.txt', essay_file='essay.txt'):
     spinner = Spinner(synonym_file)
     
-    with open(essay_file, 'r') as f:
-        original_text = clean_text(f.read())
-        print("Original:", original_text)
-        
-        results = []
-        for i in range(3):
-            spun_text = spinner.spin_text(original_text)
-            print(f"Option {i + 1}:", spun_text)
-            results.append(spun_text)
+    try:
+        with open(essay_file, 'r') as f:
+            original_text = clean_text(f.read())
+            print("Original:", original_text)
+            
+            results = []
+            for i in range(3):
+                spun_text = spinner.spin_text(original_text)
+                print(f"Option {i + 1}:", spun_text)
+                results.append(spun_text)
 
-    
-    with open('results.txt', 'w') as f:
-        for idx, result in enumerate(results, 1):
-            f.write(f"Option {idx}: {result}\n")
+        
+        with open('results.txt', 'w') as f:
+            for idx, result in enumerate(results, 1):
+                f.write(f"Option {idx}: {result}\n")
+                
+    except Exception as e:
+        print(f"Error processing text: {e}")
 
 if __name__ == '__main__':
     synonym_file = sys.argv[1] if len(sys.argv) > 1 else 'test-synonyms.txt'
