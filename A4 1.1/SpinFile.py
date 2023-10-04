20ebd4bba49cd7d33e315a5b45f3735ed85d3dbc
Meytrytosleep <145814468+Meytrytosleep@users.noreply.github.com>
2023-10-03 23:26:30 -0600

    FinalUpdate2 SpinFile.py
    forget import

--- A4 1.1/SpinFile.py
+++ A4 1.1/SpinFile.py
@@ -1,30 +1,26 @@
 import string
 import sys
+from Spinner import Spinner
 
 def clean_text(text):
     return text.translate(str.maketrans('', '', string.punctuation)).lower()
 
 def main(synonym_file='test-synonyms.txt', essay_file='essay.txt'):
     spinner = Spinner(synonym_file)
-    
-    try:
-        with open(essay_file, 'r') as f:
-            original_text = clean_text(f.read())
-            print("Original:", original_text)
-            
-            results = []
-            for i in range(3):
-                spun_text = spinner.spin_text(original_text)
-                print(f"Option {i + 1}:", spun_text)
-                results.append(spun_text)
+    with open(essay_file, 'r') as f:
+        original_text = clean_text(f.read())
+        print("Original:", original_text)
 
-        
-        with open('results.txt', 'w') as f:
-            for idx, result in enumerate(results, 1):
-                f.write(f"Option {idx}: {result}\n")
-                
-    except Exception as e:
-        print(f"Error processing text: {e}")
+        results = [original_text]
+        for i in range(3):
+            spun_text = spinner.spin_text(original_text)
+            print(f"Option {i + 1}:", spun_text)
+            results.append(spun_text)
+
+    with open('results.txt', 'w') as f:
+        for idx, result in enumerate(results):
+            label = "Original" if idx == 0 else f"Option {idx}"
+            f.write(f"{label}: {result}\n")
 
 if __name__ == '__main__':
     synonym_file = sys.argv[1] if len(sys.argv) > 1 else 'test-synonyms.txt'
